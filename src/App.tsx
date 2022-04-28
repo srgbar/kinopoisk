44426da41f792c7e98149d7bf06acacb3fd0eb20
srgbar <srg.bar@yandex.ru>
2022-04-28 14:20:50 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Search} from "./Search/Search";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Kino Poisk</h1>
+            </header>
+            <Search/>
+            <main>Films</main>
+            <footer className="footer">
+                <p>Used by<a href="http://www.omdbapi.com/" target={'blank'}> OMDB API</a></p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
